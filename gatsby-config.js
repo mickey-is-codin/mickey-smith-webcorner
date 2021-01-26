9897af407ad016b2700f76ca7d3af81fc5a5c1e7
Mickey Smith <smithmick14@gmail.com>
2021-01-26 10:46:49 -0600

    Site generation with md

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,5 +14,13 @@ module.exports = {
       },
       __key: "pages",
     },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "markdown-pages",
+        path: `${__dirname}/src/markdown-pages`,
+      },
+    },
+    "gatsby-transformer-remark",
   ],
 };
---
+++ src/markdown-pages/first-test.md
@@ -0,0 +1,11 @@
+---
+slug: /blog/first-post
+date: 2021-01-26
+title: First Test Post
+---
+
+Testing this out
+
+ - Post One
+ - Post Two
+ - Post Three
