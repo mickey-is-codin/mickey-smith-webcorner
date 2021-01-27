2352b3b7aad1096aec79589d5386b1bf6b4f3fd0
Mickey Smith <smithmick14@gmail.com>
2021-01-26 21:12:44 -0600

    Added react-icons

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+import { GiHamburgerMenu } from 'react-icons/gi';
 
 export const Navbar: React.FC = () => {
   return (
@@ -11,7 +10,7 @@ export const Navbar: React.FC = () => {
       <div className="flex-initial mx-4 bg-green-400">Resume</div>
       <div className="flex-initial mx-4 bg-green-400"><a href="/blog/first-post">First Post</a></div>
       <div className="flex-initial mx-4 bg-green-400">
-        
+        <GiHamburgerMenu />
       </div>
     </div>
   );
