f09a027fd5de36d8de82264dd336c9dece5d4082
Mickey Smith <smithmick14@gmail.com>
2021-01-28 08:21:44 -0600

    More splitting up navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,13 +2,29 @@ import React, { useState } from 'react';
 import { GiHamburgerMenu } from 'react-icons/gi';
 import { AiFillCloseCircle } from 'react-icons/ai';
 
-// Split navbars into 2 components
-// Additional component for navbar item
-
-const SiteTitle: React.FC = () => {
+interface NavbarTitleProps {
+  title: string;
+};
+const NavbarTitle: React.FC<NavbarTitleProps> = (props) => {
+  const { title } = props;
   return (
     <div className="flex-0 flex flex-col my-2 mx-4 py-auto text-center align-middle rounded-md bg-theme-3">
-      <div className="text-xl my-auto mx-2">Mickey Smith</div>
+      <div className="text-xl my-auto mx-2">{title}</div>
+    </div>
+  );
+};
+
+interface LargeViewItemProps {
+  title: string;
+  link: string;
+};
+const LargeViewItem: React.FC<LargeViewItemProps> = (props) => {
+  const { title, link } = props;
+  return (
+    <div className="flex-0 flex flex-col hidden lg:inline-flex mx-4 rounded-md my-2">
+      <div className="text-lg my-auto mx-2">
+        <a href={link}>{title}</a>
+      </div>
     </div>
   );
 };
@@ -16,25 +32,39 @@ const SiteTitle: React.FC = () => {
 const LargeViewItems: React.FC = () => {
   return (
     <div className="flex-0 flex flex-row mx-4 text-theme-2">
-      <div className="flex-0 flex flex-col hidden lg:inline-flex mx-4 rounded-md my-2">
-        <div className="text-lg my-auto mx-2">
-          Home
-        </div>
-      </div>
-      <div className="flex-0 flex flex-col hidden lg:inline-flex mx-4 rounded-md my-2">
-        <div className="text-lg my-auto mx-2">
-          <a href="/posts">Posts</a>
-        </div>
-      </div>
-      <div className="flex-0 flex flex-col hidden lg:inline-flex mx-4 rounded-md my-2">
-        <div className="text-lg my-auto mx-2">
-          About Me
-        </div>
-      </div>
-      <div className="flex-0 flex flex-col hidden lg:inline-flex mx-4 rounded-md my-2">
-        <div className="text-lg my-auto mx-2">
-          Resume
-        </div>
+      <LargeViewItem title="Home" link="/" />
+      <LargeViewItem title="Posts" link="/posts" />
+      <LargeViewItem title="About Me" link="/" />
+      <LargeViewItem title="Resume" link="/" />
+    </div>
+  );
+};
+
+interface SmallViewItemProps {
+  title: string;
+  link: string;
+};
+const SmallViewItem: React.FC<SmallViewItemProps> = (props) => {
+  const { title, link } = props;
+  return (
+    <div className="">
+      <a href={link}>{title}</a>
+    </div>
+  );
+};
+
+interface CloseNavbarProps {
+  onClose: () => void;
+};
+const CloseNavbarButton: React.FC<CloseNavbarProps> = (props) => {
+  const { onClose } = props;
+  return (
+    <div className="flex flex-row-reverse justify-right">
+      <div className="flex-0 mx-4 my-2 p-2 rounded-md bg-black bg-opacity-50">
+        <AiFillCloseCircle
+          onClick={onClose}
+          size={36}
+        />
       </div>
     </div>
   );
@@ -54,22 +84,13 @@ const SmallViewItems: React.FC = () => {
       {navbarOpen ? (
         <div
           className="fixed top-0 left-0 h-screen w-screen z-50 text-theme-2 bg-themeBlack-2 bg-opacity-75"
-          style={{
-            backdropFilter: 'blur(2px)'
-          }}
+          style={{ backdropFilter: 'blur(2px)' }}
         >
-          <div className="flex flex-row-reverse justify-right">
-            <div className="flex-0 mx-4 my-2 p-2 rounded-md bg-black bg-opacity-50">
-              <AiFillCloseCircle
-                onClick={() => setNavbarOpen(false)}
-                size={36}
-              />
-            </div>
-          </div>
-          <div className="">Home</div>
-          <div className="">Posts</div>
-          <div className="">About Me</div>
-          <div className="">Resume</div>
+          <CloseNavbarButton onClose={() => setNavbarOpen(false)} />
+          <SmallViewItem title="Home" link="/" />
+          <SmallViewItem title="Posts" link="/posts" />
+          <SmallViewItem title="About Me" link="/" />
+          <SmallViewItem title="Resume" link="/" />
         </div>
       ) : null}
     </div>
@@ -79,7 +100,7 @@ const SmallViewItems: React.FC = () => {
 export const Navbar: React.FC = () => {
   return (
     <nav className="sticky top-0 flex flex-row justify-left bg-theme-5">
-      <SiteTitle />
+      <NavbarTitle title="Mickey Smith" />
       <LargeViewItems />
       <SmallViewItems />
     </nav>
