796ec848355593de25d47038f064ef010fdd4a15
Mickey Smith <smithmick14@gmail.com>
2021-02-01 16:15:19 -0600

    post css plugin stuff

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,34 +1,6 @@
 exports.createPages = async ({ actions, graphql, reporter }) => {
   const { createPage } = actions;
-  // const mdPostTemplate = require.resolve(`./src/templates/mdPostTemplate.tsx`);
   const mdxPostTemplate = require.resolve(`./src/templates/mdxPostTemplate.tsx`);
-  // const result = await graphql(`
-  //   {
-  //     allMdx {
-  //       edges {
-  //         node {
-  //           frontmatter {
-  //             date
-  //             slug
-  //             title
-  //           }
-  //         }
-  //       }
-  //     }
-  //     allMarkdownRemark(
-  //       sort: { order: DESC, fields: [frontmatter___date] }
-  //       limit: 1000
-  //     ) {
-  //       edges {
-  //         node {
-  //           frontmatter {
-  //             slug
-  //           }
-  //         }
-  //       }
-  //     }
-  //   }
-  // `);
 
   const result = await graphql(`
     {
@@ -52,15 +24,6 @@ exports.createPages = async ({ actions, graphql, reporter }) => {
     return;
   }
 
-  // result.data.allMarkdownRemark.edges.forEach(({ node }) => {
-  //   createPage({
-  //     path: node.frontmatter.slug,
-  //     component: mdPostTemplate,
-  //     context: {
-  //       slug: node.frontmatter.slug,
-  //     },
-  //   });
-  // });
   result.data.allMdx.edges.forEach(({ node }) => {
     createPage({
       path: node.frontmatter.slug,
