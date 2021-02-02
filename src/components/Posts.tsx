bde70c68e89349095fbd4b53e269d47f50d45860
Mickey Smith <smithmick14@gmail.com>
2021-02-02 15:57:29 -0600

    Small styling

--- src/components/Posts.tsx
+++ src/components/Posts.tsx
@@ -10,12 +10,12 @@ export const Posts: React.FC = (props) => {
   return (
     <div>
       <h1 className="text-3xl">Posts</h1>
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
+      <div className="grid grid-cols-1 lg:grid-cols-2">
         {nodes.map((node: any) => {
           const { frontmatter: { title, slug, date, description } } = node;
           return (
             <div 
-              className="transition duration-500 ease-in-out bg-theme-5 rounded-md h-24 m-4 hover:bg-green-700 hover:shadow-lg cursor-pointer transform hover:-translate-y-1 hover:-translate-x-1"
+              className="transition duration-500 ease-in-out bg-theme-5 rounded-md h-24 m-4 p-2 hover:bg-green-700 hover:shadow-lg cursor-pointer transform hover:-translate-y-1 hover:-translate-x-1"
               onClick={(e) => {
                 e.preventDefault();
                 window.location.href=slug;
