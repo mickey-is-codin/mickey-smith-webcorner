c258e9e2bf9e9f86355a7d638726a2d7455a351a
Mickey Smith <smithmick14@gmail.com>
2021-02-03 09:44:30 -0600

    Homepage styling

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,10 +3,9 @@ import { graphql } from 'gatsby';
 import PageContainer from '../components/PageContainer';
 import Posts from '../components/Posts';
 
-// General overview of me and contact info
-// Web philosophy
-// Hire me page that leads to resume page or download
 // Markdown styling better
+// Class unification
+// Contact info and social media links
 
 const Index: React.FC = (props) => {
   const postsLink = <a href="/posts" className="text-white hover:text-green-500">posts I've written</a>
@@ -16,8 +15,8 @@ const Index: React.FC = (props) => {
   return (
     <PageContainer>
       <h1 className="text-3xl">Mickey Smith - Software Developer</h1>
-      <div className="flex bg-themeBlack-2 rounded-lg my-8 p-8">
-        <h2 className="flex-1 text-2xl my-auto text-center">A personal site that has ...</h2>
+      <div className="flex flex-col sm:flex-row bg-themeBlack-2 rounded-lg my-8 p-8">
+        <h2 className="flex-1 text-2xl my-auto text-center">A personal site for ...</h2>
         <div className="flex-1 my-auto text-center">
           <p className="my-2">{postsLink}</p>
           <p className="my-2">{sitesLink}</p>
--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -21,7 +21,7 @@ export const Projects: React.FC = () => {
   return (
     <PageContainer>
       <h1 className="text-3xl">Projects</h1>
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
+      <div className="grid grid-cols-1 lg:grid-cols-2">
        {PROJECTS.map((project, ix) => <ProjectCard key={`project-${ix}`} {...project} />)}
       </div>
     </PageContainer>
