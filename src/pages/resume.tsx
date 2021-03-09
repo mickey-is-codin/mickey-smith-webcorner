bd65bce7c091ffd5bfa98fbf2049c560e028d38a
Mickey Smith <smithmick14@gmail.com>
2021-03-09 10:39:48 -0600

    Resume fix

--- src/pages/resume.tsx
+++ src/pages/resume.tsx
@@ -3,6 +3,7 @@ import PageContainer from '../components/PageContainer';
 import { GrDocumentDownload } from 'react-icons/gr';
 import { RESUME, RESUME_METADATA } from '../util/constants';
 import { Experience } from '../util/types';
+import resumeFile from '../downloads/mickey_smith_resume.pdf';
 
 export const ExperienceCard: React.FC<Experience> = (props) => {
   const {
@@ -35,7 +36,7 @@ export const Resume: React.FC = () => {
         <h1 className="flex-0 text-3xl my-auto">Resume</h1>
         <a 
           className="flex-0 ml-auto p-4 transition duration-500 ease-in-out bg-theme-5 rounded-md hover:bg-green-700 hover:shadow-lg cursor-pointer transform hover:-translate-y-1 hover:-translate-x-1"
-          href={`mickey_smith_resume.pdf`}
+          href={resumeFile}
           download
         >
           <GrDocumentDownload
